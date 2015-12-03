d67bfd525c8135c02f6f35b1ff0aebdd59ae0209
shramee <shramee.srivastav@gmail.com>
2015-12-03 21:33:19 +0530

    Updating slug to 18-tags

--- inc/init.php
+++ inc/init.php
@@ -71,6 +71,6 @@ if ( is_woocommerce_activated() ) {
  */
 require 'admin/update.php';
 $example_update_checker = new ThemeUpdateChecker(
-    'eighteen-tags',
+    '18-tags',
     'http://pootlepress.github.io/18-Tags/info.json'
 );
