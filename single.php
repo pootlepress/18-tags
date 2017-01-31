b175f37e7773a280514139efbb118e457c1f068f
Shramee Srivastav <shramee.srivastav@gmail.com>
2017-01-31 17:05:28 +0530

    Fixing pootlepress/eighteen-tags-pro#49

--- single.php
+++ single.php
@@ -14,55 +14,57 @@
 get_header();
 include get_template_directory() . '/inc/styles.php';
 # region Title on top
-$layout = get_option( 'etp_post_layout' );
-if ( 'title-top' == $layout ) {
-	?>
-	<div class="col-full">
-		<?php
-		the_title( '<h1 class="entry-title" itemprop="name headline">', '</h1>' );
-		if ( ! get_theme_mod( 'eighteen-tags-pro-remove-single-post-meta' ) ) {
-			?>
-			<div class="post-meta-container">
-				<?php
-				eighteen_tags_posted_on();
-				eighteen_tags_post_meta();
-				?>
-			</div>
-			<?php
-		}
-		?>
-	</div><!-- .col-full -->
-	<?php
-}
-# endregion
-if ( ! pootlepb_is_panel() ) {
-	?>
-	<header class="entry-header">
-		<?php
-		if ( has_post_thumbnail( get_the_ID() ) ) {
-			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-			$class = "class='etp-full-width-image-bg'";
-			echo "<div $class style=\"background-image:url('$image[0]')\"></div>";
-			?>
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post();
 
-			<?php
-		} else {
+		$layout = get_option( 'etp_post_layout' );
+		if ( 'title-top' == $layout ) {
 			?>
-			<div class="margin-bottom"></div>
+			<div class="col-full">
+				<?php
+				the_title( '<h1 class="entry-title" itemprop="name headline">', '</h1>' );
+				if ( ! get_theme_mod( 'eighteen-tags-pro-remove-single-post-meta' ) ) {
+					?>
+					<div class="post-meta-container">
+						<?php
+						eighteen_tags_posted_on();
+						eighteen_tags_post_meta();
+						?>
+					</div>
+					<?php
+				}
+				?>
+			</div><!-- .col-full -->
+			<?php
+		}
+# endregion
+		if ( ! pootlepb_is_panel() ) {
+			?>
+			<header class="entry-header">
+				<?php
+				if ( has_post_thumbnail( get_the_ID() ) ) {
+					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+					$class = "class='etp-full-width-image-bg'";
+					echo "<div $class style=\"background-image:url('$image[0]')\"></div>";
+					?>
+
+					<?php
+				} else {
+					?>
+					<div class="margin-bottom"></div>
+					<?php
+				}
+				?>
+			</header><!-- .entry-header -->
 			<?php
 		}
 		?>
-	</header><!-- .entry-header -->
-	<?php
-}
-?>
-<div class="col-full">
-<div id="primary" class="content-area etp-awesome-layout-1">
-	<main id="main" class="site-main" role="main">
-		<?php if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope="" itemtype="http://schema.org/BlogPosting">
+		<div class="col-full">
+		<div id="primary" class="content-area etp-awesome-layout-1">
+			<main id="main" class="site-main" role="main">
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope=""
+				         itemtype="http://schema.org/BlogPosting">
 					<?php
 					# region title not on top
 					if ( 'title-top' != $layout ) {
@@ -102,32 +104,31 @@ if ( ! pootlepb_is_panel() ) {
 					do_action( 'eighteen_tags_single_post_after' );
 					?>
 				</div><!-- .col-full -->
-				<?php
-			endwhile;
-		else : ?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
-
-	<?php
-	if ( get_theme_mod( 'eighteen-tags-pro-single-keep-sidebar' ) ) {
-		do_action( 'eighteen_tags_sidebar' );
-	} else {
-		?>
-		<style>
-			@media only screen and (min-width: 770px) {
-				.eighteen-tags-pro-active #primary {
-					width: 100%;
-				}
-			}
-		</style>
 		<?php
-	}
+	endwhile;
+else : ?>
+
+	<?php get_template_part( 'content', 'none' ); ?>
+
+<?php endif; ?>
+<?php
+if ( get_theme_mod( 'eighteen-tags-pro-single-keep-sidebar' ) ) {
+	do_action( 'eighteen_tags_sidebar' );
+} else {
 	?>
-</div>
+	<style>
+		@media only screen and (min-width: 770px) {
+			.eighteen-tags-pro-active #primary {
+				width: 100%;
+			}
+		}
+	</style>
+	<?php
+}
+?>
+	</div>
 <?php
 get_footer();
