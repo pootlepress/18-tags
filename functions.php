950b8d817884fe42565f5ec20b80b2ed5330a50a
Shramee Srivastav <shramee.srivastav@gmail.com>
2018-03-12 17:08:08 +0530

    Ready for v2.7.1

--- functions.php
+++ functions.php
@@ -6,52 +6,7 @@
  * @developer shramee <shramee.srivastav@gmail.com>
  */
 
-define( 'EIGHTEENTAGS_VERSION', '2.7.0' );
-/*
-// Create a helper function for easy SDK access.
-function eighteen_tags_fs() {
-	global $eighteen_tags_fs;
-
-	if ( ! isset( $eighteen_tags_fs ) ) {
-		// Include Freemius SDK.
-		require_once get_template_directory() . '/wp-sdk/start.php';
-
-		$eighteen_tags_fs = fs_dynamic_init( array(
-			'id'             => '1053',
-			'slug'           => 'eighteen-tags',
-			'type'           => 'theme',
-			'public_key'     => 'pk_9e49400ad7036836549c80945771d',
-			'is_premium'     => false,
-			'has_addons'     => false,
-			'has_paid_plans' => false,
-			'menu'           => array(
-				'slug'           => 'eighteen-tags-welcome',
-				'override_exact' => true,
-				'support'        => false,
-				'parent'         => array(
-					'slug' => 'themes.php',
-				),
-			),
-		) );
-    }
-
-    return $eighteen_tags_fs;
-}
-
-// Init Freemius.
-eighteen_tags_fs();
-// Signal that SDK was initiated.
-do_action( 'eighteen_tags_fs_loaded' );
-
-function eighteen_tags_fs_settings_url() {
-    return admin_url( 'themes.php?page=eighteen-tags-welcome' );
-}
-
-eighteen_tags_fs()->add_filter( 'connect_url', 'eighteen_tags_fs_settings_url' );
-eighteen_tags_fs()->add_filter( 'after_skip_url', 'eighteen_tags_fs_settings_url' );
-eighteen_tags_fs()->add_filter( 'after_connect_url', 'eighteen_tags_fs_settings_url' );
-eighteen_tags_fs()->add_filter( 'after_pending_connect_url', 'eighteen_tags_fs_settings_url' );// Signal that SDK was initiated.
-*/
+define( 'EIGHTEENTAGS_VERSION', '2.7.1' );
 
 /**
  * Initialize all the things.
--- readme.txt
+++ readme.txt
@@ -8,7 +8,7 @@ Donate link:
 
 Requires at least: 4.0
 Tested up to: 4.5.2
-Stable tag: 2.7.0
+Stable tag: 2.7.1
 
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
